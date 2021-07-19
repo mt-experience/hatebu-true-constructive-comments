48880877fd93a1c4bc3234221aa7102f93b0e64a
mt-experience <pc37458@gmail.com>
2021-07-19 21:07:58 +0900

    表示だけ

---
+++ docs/script.js
@@ -0,0 +1,7 @@
+'use strict'
+window.addEventListener("load", function(){
+  const btn = document.getElementById("show-button")
+  const input = document.getElementById("url-input")
+  const area = document.getElementById("comment-area")
+  
+})
